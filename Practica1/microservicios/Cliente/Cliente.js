1c1682c43172e783ad80cd8c9c2cad22dcdf0b2c
Armando Guix <javierarmandoguix@gmail.com>
2022-01-31 23:11:37 -0600

    Servicio de clientes

--- Practica1/microservicios/Cliente/Cliente.js
+++ Practica1/microservicios/Cliente/Cliente.js
@@ -1,4 +1,4 @@
-/*const { Router } = require('express');
+const { Router } = require('express');
 const jwt = require("jsonwebtoken");
 const router = Router();
 
@@ -8,4 +8,30 @@ router.get("/apiCliente", async(req, res)=>{
 	});
 });
 
-module.exports = router;*/
+//Perimitiremos al usuario acceder a la ruta siempre que tenga el token
+router.post("/api/testCliente", VerificarToken, async(req, res)=>{	
+	//Verificar Token
+	jwt.verify(req.token, 'llave', (error, authData)=>{
+		if(error){
+			console.log("Token Incorrecto");
+			res.sendStatus(403);
+		}else{
+			res.json({
+				mensaje: "Token Correcto, Bienvenido al servicio de cliente"			
+			});
+		}
+	});
+});
+
+// Authorization: Bearer <token>
+function VerificarToken(req, res, next){	
+	const beareheader = req.headers['authorization'];
+	if(typeof beareheader !== 'undefined'){
+		const token = beareheader.split(" ")[1];
+		req.token = token;
+		next();
+	}else{		
+		res.sendStatus(403);
+	}
+}
+module.exports = router;
--- Practica1/microservicios/Login/app.js
+++ Practica1/microservicios/Login/app.js
@@ -8,7 +8,7 @@ app.use(bp.urlencoded({ extended: false }))
 
 //Routes
 app.use(require('./Login.js'));
-//app.use(require('../Cliente/Cliente.js'));
+app.use(require('../Cliente/Cliente.js'));
 
 app.use(cors());
 app.use(express.json({ limit: '100mb' }))
